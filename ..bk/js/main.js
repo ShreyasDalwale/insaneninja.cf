6c2029884572d94ab8d046e3d7e90c3a9dd503c3
insane-ninja <52408859+insane-ninja@users.noreply.github.com>
2019-12-08 19:37:03 +0530

    update

---
+++ ..bk/js/main.js
@@ -0,0 +1,10 @@
+function menuopen() {
+	var x = document.getElementById('menuopen');
+	x.style.display="block";
+	// alert("hello");
+}
+function menuclose() {
+	var x = document.getElementById('menuopen');
+	x.style.display="none";
+	// alert("hello");
+}
